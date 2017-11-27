5bfe0c90e56bd1b895e049a4835803dad0944563
houcunlu <544845389@qq.com>
2017-11-27 14:07:39 +0800

    table

--- table/1.0/codingapi-table.js
+++ table/1.0/codingapi-table.js
@@ -2,29 +2,27 @@
  * Created by houcunlu on 2017/11/25.
  */
 
-URL = 'http://127.0.0.1:8080/xx/';
-
 var defaultAttrs = [
-        {name:'api-data-method',value:"post"},
-        {name:'api-data-toggle',value:"table"},
-        {name:'api-data-height',value:"600"},
-        {name:'api-data-side-pagination',value:"server"},
-        {name:'api-data-pagination',value:"true"},
-        {name:'api-data-page-list',value:"[5, 10, 20, 50, 100, 200]"},
-        {name:'api-data-search',value:"true"},
-        {name:'api-data-show-refresh',value:"true"},
-        {name:'api-data-show-toggle',value:"true"},
-        {name:'api-data-show-columns',value:"true"}
-    ];
+    {name:'api-data-method',value:"post"},
+    {name:'api-data-toggle',value:"table"},
+    {name:'api-data-height',value:"600"},
+    {name:'api-data-side-pagination',value:"server"},
+    {name:'api-data-pagination',value:"true"},
+    {name:'api-data-page-list',value:"[5, 10, 20, 50, 100, 200]"},
+    {name:'api-data-search',value:"true"},
+    {name:'api-data-show-refresh',value:"true"},
+    {name:'api-data-show-toggle',value:"true"},
+    {name:'api-data-show-columns',value:"true"}
+];
 
 
 var constant = function (attrName,attrs) {
     for(var i in attrs) {
         var attr = attrs[i];
         var name = attr.name;
-         if(attrName == name){
-             return true;
-         }
+        if(attrName == name){
+            return true;
+        }
     }
     return false;
 }
@@ -50,13 +48,9 @@ var init = function(){
 
     var tabs =$("div[api-type=table]");
     $.each(tabs,function(){
-
         var div = $(this);
-
         var attrs = reloadNamedNodeMap(div[0].attributes);
-
         var table = $('<table></table>');
-
         for(var i in defaultAttrs){
             var attr = defaultAttrs[i];
             var attrName = attr.name;
@@ -76,30 +70,45 @@ var init = function(){
                 table.attr(newAttrName,attrValue);
             }
         }
-       $(div).append(table);
-
+        $(div).append(table);
+        getTableStyle($(div))
     });
 }
 
 
 /**
- *  初始化table 头部 【 div中需添加 api-data-table-id 属性 】
- *  tableId
- *  head ： 为json格式 ["id","名字","密码"] 字符串
+ *  初始化table 头部
  * @param tableId
  */
-var getTableStyle = function( tableId , head ){
-    var thead = $('<thead></thead>');
-    var tr = $('<tr></tr>');
+var getTableStyle = function( div ){
+    var url =    $(div).attr("api-url");
+    var id =  $(div).attr("api-data-id");
+    var table = $("table[data-id="+id+"]");
+    var tableUrl ="";
 
-    var obj = JSON.parse(head);
+    $.ajaxSettings.async = false;
+    $.getJSON(url, function (data){
+        var thead = $('<thead></thead>');
+        var tr = $('<tr></tr>');
+        for(var i = 0; i< data.filed.length ; i++){
+            var obj = data.filed[i];
+            var method ="";
+            if(obj.method !=""){
+                method="data-formatter=\""+obj.method+"\"";
+            }
+            tr.append('<th  data-field='+obj.value+'  '+method+'>'+obj.name+'</th>');
+        }
+        thead.append(tr);
+        tableUrl = data.dataUrl;
+        $(table).append(thead);
+    });
 
-    for(var i = 0; i< obj.length ; i++){
-        tr.append('<th>'+obj[i]+'</th>');
-    }
-    thead.append(tr);
-    $("table[data-table-id="+tableId+"]").append(thead);
-
+    setTimeout(function(){
+        var token =   comment.getToken();
+        var url = URL + tableUrl+"?token="+token;
+        table.bootstrapTable("refresh", {url: url});
+    },1);
 }
+
 
 init();
